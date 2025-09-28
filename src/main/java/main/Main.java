ab3b6d5dbfdef8c9c5ebc028cbf72174a24f7054
Egor Koshelev <ekoshelev@ibsys.ru>
2025-09-28 17:36:55 +0300

    TEST

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -22,6 +22,7 @@ public class Main {
             System.out.println("Общая сумма товаров со скидкой:" + shoppingcart.amountDiscountedItems() + "рублей");
             System.out.println("Сумма вегетарианских продуктов без скидки:" + shoppingcart.sumVeganproducts() + "рублей");
         }
-    }
+
+}
 
 
